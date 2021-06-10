47297c169c54086a486ff3598427311d9267d682
VishalSingh28 <visusingh009@gmail.com>
2021-06-10 18:47:43 +0530

    material commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { StepperOrientation} from '@angular/material/stepper';
+import {FormBuilder, Validators} from '@angular/forms';
+import {BreakpointObserver} from '@angular/cdk/layout';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'UpSkill';
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required]
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required]
+  });
+  thirdFormGroup = this._formBuilder.group({
+    thirdCtrl: ['', Validators.required]
+  });
+  stepperOrientation: Observable<StepperOrientation>;
+
+  constructor(private _formBuilder: FormBuilder, breakpointObserver: BreakpointObserver) {
+    this.stepperOrientation = breakpointObserver.observe('(min-width: 800px)')
+      .pipe(map(({matches}) => matches ? 'horizontal' : 'vertical'));
+  }
+}
+
+
